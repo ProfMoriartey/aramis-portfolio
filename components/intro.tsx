79e53c2cf258c6228d16865333d1b05565d102fe
profmoriartey <kaito9814@gmail.com>
2023-08-30 20:03:31 +0300

    Add more changes to the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -19,7 +19,7 @@ export default function Intro() {
       id="home"
       className="flex flex-col sm:flex-row justify-center items-center mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className="pb-5">
+      <div className="flex flex-reverse">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -35,7 +35,7 @@ export default function Intro() {
             height="2000"
             quality="95"
             priority={true}
-            className=""
+            className="mb-0 sm:mb-20 sm:mr-10"
           />
         </motion.div>
       </div>
@@ -63,7 +63,7 @@ export default function Intro() {
         >
           <Link
             href="#contact"
-            className="group bg-gray-900 text-white mr-2.5 px-7 py-3 flex items-center gap-4 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+            className="group bg-[#5e434a] text-white mr-2.5 px-7 py-3 flex items-center gap-4 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
             onClick={() => {
               setActiveSection("Contact");
               setTimeOfLastClick(Date.now());
@@ -72,23 +72,24 @@ export default function Intro() {
             Contact me here{" "}
             <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
           </Link>
+          <div className="flex gap-2">
+            <a
+              className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+              href="/CV.pdf"
+              download
+            >
+              Download CV{" "}
+              <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
 
-          <a
-            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-            href="/CV.pdf"
-            download
-          >
-            Download CV{" "}
-            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-          </a>
-
-          <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://www.linkedin.com/in/aramis-soto-494b57272/"
-            target="_blank"
-          >
-            <BsLinkedin />
-          </a>
+            <a
+              className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+              href="https://www.linkedin.com/in/aramis-soto-494b57272/"
+              target="_blank"
+            >
+              <BsLinkedin />
+            </a>
+          </div>
         </motion.div>
       </div>
     </section>
